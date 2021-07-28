68899f9d55c23fad5d5339bd30c8b878ecf8e6a3
Sergio Barria <sergio.barria9210@gmail.com>
2021-07-27 23:26:50 -0500

    add layout, navbar, hero, services

--- 02-portfolio-template/src/components/Services.js
+++ 02-portfolio-template/src/components/Services.js
@@ -1,8 +1,23 @@
-import React from "react"
-import Title from "./Title"
-import services from "../constants/services"
+import React from 'react'
+import Title from './Title'
+import services from '../constants/services'
+
 const Services = () => {
-  return <h2>services component</h2>
+  return (
+    <section className='section bg-grey'>
+      <Title title='services' />
+      <div className='section-center services-center'>
+        {services.map(service => (
+          <article key={service.id} className='service'>
+            {service.icon}
+            <h4>{service.title}</h4>
+            <div className='underline'></div>
+            <p>{service.text}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default Services
