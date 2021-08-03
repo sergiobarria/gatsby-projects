44b2ac4525bc0b1ef86ccd872dcc03ba1bdd20a7
Sergio Barria <sergio.barria9210@gmail.com>
2021-08-02 22:56:24 -0500

    add featured posts, posts page and single post programatically

--- 03-mdx-blog/src/pages/index.js
+++ 03-mdx-blog/src/pages/index.js
@@ -4,10 +4,39 @@ import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 
-const IndexPage = () => {
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        frontmatter {
+          author
+          category
+          readTime
+          slug
+          title
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title='recently published' />
     </Layout>
   )
 }
--- 03-mdx-blog/src/pages/posts.js
+++ 03-mdx-blog/src/pages/posts.js
@@ -4,8 +4,41 @@ import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
 
-const PostsPage = () => {
-  return <h4>posts page</h4>
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        frontmatter {
+          author
+          category
+          readTime
+          slug
+          title
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
+
+const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+
+  return (
+    <Layout>
+      <Hero />
+      <Posts posts={posts} title='all posts' />
+    </Layout>
+  )
 }
 
 export default PostsPage
